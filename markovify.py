ab5b07242848788d9a3013c32b82edae35ee0081
Kostya Vakarian <vakarianplay@gmail.com>
2021-01-29 13:23:13 +0300

    Update markovify.py

--- markovify.py
+++ markovify.py
@@ -3,5 +3,7 @@ import markovify
 with open("qq.txt",encoding='utf-8') as f:
     text = f.read()
 text_model = markovify.Text(text)
+for i in range(5):
+    print(text_model.make_sentence())
 for i in range(10):
     print(text_model.make_short_sentence(380))
