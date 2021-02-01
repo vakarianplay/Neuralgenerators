b57f85ab40978fc5f7da682e2736947afac6671e
Kostya Vakarian <vakarianplay@gmail.com>
2021-02-01 11:15:29 +0300

    Update markovify.py

--- markovify.py
+++ markovify.py
@@ -1,7 +1,9 @@
 import markovify
 
-with open("qq.txt",encoding='utf-8') as f:
+#file.txt - your dataset file
+with open("file.txt",encoding='utf-8') as f:
     text = f.read()
+    
 text_model = markovify.Text(text)
 for i in range(5):
     print(text_model.make_sentence())
